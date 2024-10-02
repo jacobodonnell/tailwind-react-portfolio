57cd5340a73ad0b27cd448434e034e74559dc695
Jacob O'Donnell <jacobodonnell96@proton.me>
2024-10-02 18:13:42 -0500

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Skills from './components/Skills';
+import About from './components/About';
+import Projects from './components/Projects';
+
 const App = () => {
-  return <h1 className="font-thin  text-7xl text-zinc-300">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <About />
+      <Projects />
+    </>
+  );
 };
 export default App;
